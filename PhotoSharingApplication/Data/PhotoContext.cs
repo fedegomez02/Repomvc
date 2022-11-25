c794b94c2b08fc39ac20b31fbb3b891bfc0398bd
Fedee Gomez drees <abcrojo5@gmail.com>
2022-11-25 15:59:49 -0300

    a

---
+++ PhotoSharingApplication/Data/PhotoContext.cs
@@ -0,0 +1,18 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoContext:DbContext
+    {
+        public PhotoContext():base ("PhotoKeyDB") { }
+
+        public DbSet<Photo> fotos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
